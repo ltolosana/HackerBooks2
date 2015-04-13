cfe95c8e85ec4a27b1db24238cd078fea2b1f8a0
Luis Mª Tolosana <ltolosana@gmail.com>
2015-04-13 20:02:53 +0200

    Initial Commit

---
+++ HackerBooks2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerBooks2
+//
+//  Created by Luis M Tolosana Simon on 13/4/15.
+//  Copyright (c) 2015 Luis M Tolosana Simon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
